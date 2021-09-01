5104123e24f5736bb1d51f80df620019304c0a00
lazy <641541452@qq.com>
2021-09-01 21:02:24 +0800

    9.1 21:02

--- leetcode/offerIsComing/查找算法/剑指 Offer 11. 旋转数组的最小数字.py
+++ leetcode/offerIsComing/查找算法/剑指 Offer 11. 旋转数组的最小数字.py
@@ -10,7 +10,6 @@
 输出：0
 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
 """
-from typing import List
 
 
 class Solution:
@@ -19,11 +18,27 @@ class Solution:
         while i < j:
             m = (i + j) // 2
             if numbers[m] > numbers[j]:
+                # 这里是因为numbers[m]不可能是旋转点
                 i = m + 1
             elif numbers[m] < numbers[j]:
+                # 这里是因为numbers[m]也可能是旋转店
                 j = m
             else:
                 # return min(numbers[i:j])
+                j -= 1
+        return numbers[i]
+
+
+class Solution1:
+    def minArray(self, numbers: [int]) -> int:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            mid = (i + j) // 2
+            if numbers[mid] > numbers[j]:
+                i = mid + 1
+            elif numbers[mid] < numbers[j]:
+                j = mid
+            else:
                 j -= 1
         return numbers[i]
 
@@ -37,9 +52,9 @@ if __name__ == "__main__":
     循环二分： 设 m = (i + j) / 2为每次二分的中点（ "/" 代表向下取整除法，因此恒有 i≤m<j ），可分为以下三种情况：
     当 nums[m] > nums[j]时： m 一定在 左排序数组 中，即旋转点 x 一定在 [m + 1, j] 闭区间内，因此执行 i = m + 1；
     当 nums[m] < nums[j]时： m 一定在 右排序数组 中，即旋转点 x 一定在[i, m] 闭区间内，因此执行 j = m；
-    当 nums[m] = nums[j]时： 无法判断 m 在哪个排序数组中，即无法判断旋转点 x 在 [i, m]
-    还是 [m + 1, j]区间中。解决方案： 执行 j = j - 1 缩小判断范围，分析见下文。
-    返回值： 当 i = j时跳出二分循环，并返回 旋转点的值 nums[i] 即可。
+    当 nums[m] = nums[j]时： 无法判断 m 在哪个排序数组中，即无法判断旋转点 x 在 [i, m]还是 [m + 1, j]区间中。
+    解决方案： 执行 j = j - 1 缩小判断范围，分析见下文。
+    返回值： 当 i = j时跳出二分循环，并返回旋转点的值 nums[i] 即可。
     
     实际上，当出现 nums[m] = nums[j] 时，一定有区间 [i, m]内所有元素相等 或 区间 [m, j]内所有元素相等（或两者皆满足）。
     对于寻找此类数组的最小值问题，可直接放弃二分查找，而使用线性查找替代。
@@ -47,5 +62,7 @@ if __name__ == "__main__":
     """
     A = Solution()
     nums = [3, 4, 5, 1, 2, 3]
-
+    print(A.minArray(nums))
+    A = Solution1()
+    nums = [3, 4, 5, 1, 2, 3]
     print(A.minArray(nums))
